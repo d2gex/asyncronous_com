946ba41a8bccc80498b6ffbf13ddb2ec5dfea6c1
d2gex <daniel.garcia@d2garcia.com>
2019-07-21 21:14:52 +0100

    Fix stop=True when worker receive DIE

--- asyncronous_com/worker.py
+++ asyncronous_com/worker.py
@@ -9,6 +9,7 @@ class Worker(IProcess):
 
     def __init__(self, identity, url, conn, parent_id, app, linger=0):
         super().__init__()
+        self.identity = identity
         self.pid = multiprocessing.current_process().pid
         self.parent_id = parent_id
         self.conn = conn
@@ -17,24 +18,30 @@ class Worker(IProcess):
 
     def run(self, loops=True):
 
-        stop = False
-        while not stop and loops:
+        try:
+            stop = False
+            while not stop and loops:
 
-            # (1) is the task addressed to me?
-            task = self.conn.receive(func=lambda sms: sms[mpq_protocol.R_PID_OFFSET] in (self.pid, None))
-            if task:
-                # (1.1) Is it a poison pill? => time pass away
-                if task[mpq_protocol.S_PID_OFFSET - 1] == mpq_protocol.REQ_DIE:
-                    stop = False
-                # (1.2) Send the information via my producer and tell my parent that I am done with it
-                else:
-                    result = self.app(task[-1])
-                    self.producer.run(result)
-                    self.conn.send(mpq_protocol.REQ_FINISHED, recipient_pid=self.parent_id)
+                # (1) is the task addressed to me?
+                task = self.conn.receive(func=lambda sms: sms[mpq_protocol.R_PID_OFFSET] in (self.pid, None))
+                # print(f"{self.identity}: {task}")
+                if task:
+                    # (1.1) Is it a poison pill? => time pass away
+                    if task[mpq_protocol.S_PID_OFFSET - 1] == mpq_protocol.REQ_DIE:
+                        stop = True
+                    # (1.2) Send the information via my producer and tell my parent that I am done with it
+                    else:
+                        result = self.app(task[-1])
+                        self.producer.run(result)
+                        self.conn.send(mpq_protocol.REQ_FINISHED, recipient_pid=self.parent_id)
 
-            # (2) Do we have a finite mandate?
-            if not stop and not isinstance(loops, bool):
-                loops -= 1
+                # (2) Do we have a finite mandate?
+                if not stop and not isinstance(loops, bool):
+                    loops -= 1
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.clean()
 
     def clean(self):
         if self.producer:
