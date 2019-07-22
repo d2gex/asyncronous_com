29ba7eca25c7e1d0d1699f2d1467a83645e2b3f2
d2gex <daniel.garcia@d2garcia.com>
2019-07-22 07:09:03 +0100

    Add data files generator for testing purposes

---
+++ tests/data/generate_data.py
@@ -0,0 +1,13 @@
+from os.path import join
+from tests import utils as test_utils
+
+
+if __name__ == "__main__":
+    path = join(test_utils.TEST, 'data')
+    with open(join(path, 'server_data.txt'), 'w+') as fh:
+        for item in test_utils.generate_server_data('137.72.95', 3, response=1, wait=1):
+            fh.write(item + '\n')
+
+    with open(join(path, 'client_data.txt'), 'w+') as fh:
+        for item in test_utils.generate_client_data('137.72.95', 3):
+            fh.write(item + '\n')
